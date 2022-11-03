7ade74e62c27fd472f00918b239146cb8339e41d
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2022-11-04 01:05:42 +0300

    HW

---
+++ Task1/script.js
@@ -0,0 +1,9 @@
+function hello(){
+    let person = prompt('What is ur name?')
+        if (person){
+            alert(`Hello, ${person}`)
+        } else {
+            alert(`Nu, dopustium, myau...`)
+        }
+        
+}
