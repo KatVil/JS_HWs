b7ef9d203bac2cec4e384c07cccc3bd7d45a2be9
katvil <pakhomovacatherine@gmail.com>
2022-11-09 23:07:26 +0300

    CatGallerySecVer

---
+++ Week2/Task2/script.js
@@ -0,0 +1,10 @@
+function ChangeMeForward(){
+    let catImage = document.getElementById('catImage');
+    catImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgizmodo.com%2Fif-youre-owned-by-a-cat-scientists-want-to-hear-from-y-1849149263&psig=AOvVaw31o02geIkupbjdmml0pyg8&ust=1668106234975000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOC0047iofsCFQAAAAAdAAAAABBP";
+
+}
+
+function ChangeMeBackward(){
+    let catImage = document.getElementById('catImage');
+    catImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fspca.bc.ca%2Ffaqs%2Fcats%2F&psig=AOvVaw31o02geIkupbjdmml0pyg8&ust=1668106234975000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOC0047iofsCFQAAAAAdAAAAABBZ";
+}
