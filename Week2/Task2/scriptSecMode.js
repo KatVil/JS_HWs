e97fc7c5ddc8dc1fa5c92fd508642d6758476846
katvil <pakhomovacatherine@gmail.com>
2022-11-11 17:26:18 +0300

    plusDvaKoteka

--- Week2/Task2/scriptSecMode.js
+++ Week2/Task2/scriptSecMode.js
@@ -1,5 +1,5 @@
 let current = 1;
-let quantity = 11;
+let quantity = 13;
 
 cats = new Array;
 cats[0] = new Image();
@@ -13,6 +13,9 @@ cats[7] = new Image();
 cats[8] = new Image();
 cats[9] = new Image();
 cats[10] = new Image();
+cats[11] = new Image();
+cats[12] = new Image();
+
 
 cats[0].src="./assets/img/cat-217679.jpg";
 cats[1].src="./assets/img/cat.jpg";
@@ -20,12 +23,13 @@ cats[2].src="./assets/img/catcat.webp";
 cats[3].src="./assets/img/kat.webp";
 cats[4].src="./assets/img/kitten.jpg"
 cats[5].src="./assets/img/mmrmrmrm.webp"
-cats[6].src="./assets/img/mr.jpeg"
+cats[6].src="./assets/img/mr.jpg"
 cats[7].src="./assets/img/mrmr.webp"
 cats[8].src="./assets/img/mrmr.webp"
-cats[9].src="./assets/img/mur.webp"
+cats[9].src="./assets/img/mur.jpg"
 cats[10].src="./assets/img/purrr.jpg"
 cats[11].src="./assets/img/redcar.jpg";
+cats[12].src="./assets/img/mur.webp";
 
 function ChangeMeForward(){
     //if (current < quantity) {
