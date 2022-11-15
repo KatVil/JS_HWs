9016b05f4cf4247ab0064cd6886d124c02c7c62d
katvil <pakhomovacatherine@gmail.com>
2022-11-15 23:02:22 +0300

    formupdate

--- Week2/Week3/Task2/script.js
+++ Week2/Week3/Task2/script.js
@@ -26,3 +26,21 @@ function CheckPhone(){
     }
     
 }
+
+function CheckEmail(){
+    let resultEm=document.getElementById("email").value;
+    if (resultEm.HTMLElement.innerText.includes('@')){
+        console.log(resultEm);
+    }
+    else{
+        alert('Please enter the correct email');
+    }
+}
+
+function CheckPass(){
+    let resultPassOne=document.getElementById("password-input").value;
+    let resultPassTwo=document.getElementById("password-input-repeat").value;
+    if (resultPassOne != resultPassTwo){
+        alert('Passwords does not match!')
+    }    
+}
