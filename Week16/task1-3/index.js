71091fa8ee86e7dda2e183ffd2d0e32bb7b9322b
katvil <pakhomovacatherine@gmail.com>
2022-12-07 20:07:30 +0300

    w16=-trouble

--- Week16/task1-3/index.js
+++ Week16/task1-3/index.js
@@ -3,6 +3,9 @@ const models = document.querySelector('#models');
 const result = document.querySelector('.result');
 
 let audiModels = ['a1', 'a2', 'tt', 'q3'];
+let jigulModels = ['b1', 'b2', 'qt', 'q4'];
+let kiaModels = ['c1', 'c2', 'ct', 'qc'];
+let nissanModels = ['d1', 'a2', 'tt', 'd3'];
 
 const selectBrand = () => {
 
@@ -16,7 +19,22 @@ const selectBrand = () => {
     let options = ''
     if(brands.value = '300'){
         for(let model of audiModels){
-            options += `<option value='1000'>${model}</option>`
+            options += `<option value='300'>${model}</option>`
+        }     
+    }
+    else if (brands.value = '100'){
+        for(let model of jigulModels){
+            options += `<option value='100'>${model}</option>`
+        }     
+    }
+    else if (brands.value = '400'){
+        for(let model of kiaModels){
+            options += `<option value='400'>${model}</option>`
+        }     
+    }
+    else if (brands.value = '500'){
+        for(let model of nissanModels){
+            options += `<option value='500'>${model}</option>`
         }     
     }
     models.innerHTML = options
